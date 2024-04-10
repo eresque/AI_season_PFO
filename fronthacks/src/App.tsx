089e5de280ac3a0c30a48bd40e2c28b6cd865830
eresque- <69436547+eresque@users.noreply.github.com>
2024-04-10 18:31:11 +0300

    Update App.tsx

--- fronthacks/src/App.tsx
+++ fronthacks/src/App.tsx
@@ -45,7 +45,7 @@ const App = (): JSX.Element => {
     async function fetchData(input: string) {
         setMessage(true);
         try {
-            const res = await axios.get(`http://backhack:8000/pizdec?date=${input}`);
+            const res = await axios.get(`http://172.22.0.2:8000/pizdec?date=${input}`);
             console.log(res.data.event);
             setDtpData(res.data.event.dtp);
             setToxicData(res.data.event.toxic);
