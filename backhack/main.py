62d578070a7c9591b639b023ccb8554097ab828a
eresque- <69436547+eresque@users.noreply.github.com>
2024-04-10 21:53:46 +0300

    Update main.py

--- backhack/main.py
+++ backhack/main.py
@@ -18,10 +18,7 @@ app = FastAPI(
 )
 
 origins = [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://172.22.0.3:3000',
 ]
 app.add_middleware(
     CORSMiddleware,
