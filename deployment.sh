066922377e39502b430db7479bb813eb1f8235a3
Gordeev Artem <tema.gor17@gmail.com>
2024-04-11 22:37:21 +0300

    l

---
+++ deployment.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install git
+git --version
+
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+
+git clone https://github.com/eresque/AI_season_PFO.git
+
+cd AI_season_PFO
+
+
+
+ls docker-compose.yml
+sudo docker compose up
+sudo docker ps 
